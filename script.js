f9a9fa1faa8f96f5f46604270adb77fe57c40c3a
luke01455 <luke0145555@hotmail.co.uk>
2019-07-31 22:04:49 +0100

    arrow function basics and some map loop

--- script.js
+++ script.js
@@ -86,6 +86,7 @@ console.log(c);
 
 // Lecture : Strings:
 
+/*
 let firstName = 'John';
 let lastName = 'Smith';
 const yearOfBirth = 1990;
@@ -109,14 +110,32 @@ console.log(n.startsWith('J'));
 console.log(n.endsWith('th'));
 console.log(n.includes(' '));
 console.log(`${firstName} `.repeat(5));
+*/
 
+// Lecture: Arrow Functions 
 
+const years = [1990, 1965, 1982, 1937];
 
+// ES5
+var ages5 = years.map(function(el){
+    return 2016 - el;
+});
 
+console.log(ages5);
 
+// ES6
+let ages6 = years.map(el => 2016 - el);
+console.log(ages6);
 
+ages6 = years.map((el, index) => `Age element ${index + 1}: ${2016-el}.`)
+console.log(ages6);
 
-
+ages6 = years.map((el, index) => {
+const now = new Date().getFullYear();
+const age = now - el;
+return `Age element ${index + 1}: ${age}.`
+});
+console.log(ages6);
 
 
 
