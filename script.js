829b3d6707d60c55893a516f23b4c0e8c707c441
luke01455 <luke0145555@hotmail.co.uk>
2019-08-02 17:18:35 +0100

    Arrays in ES6, findIndex, from, find,

--- script.js
+++ script.js
@@ -235,7 +235,7 @@ new Person('John').myFriends6(friends);
 
 // ES6
 
-
+/*
 const John = [name, age] = ['John', 26];
 console.log(name);
 console.log(age);
@@ -268,21 +268,69 @@ console.log(age2);
 console.log(retirement);
 
 
+git commit -m "destructuring, aka splitting an array into idividual variables"
+*/
+
+///////////////////////////////
+/// Lecture: Array ES6
+
+const boxes = document.querySelectorAll('.box');
+
+// ES5
+/*
+var boxesArr5 = Array.prototype.slice.call(boxes);
+
+boxesArr5.forEach(function(cur) {
+    cur.style.backgroundColor = 'dodgerblue';
+});
+*/
+
+// ES6
+const boxesArr6 = Array.from(boxes);
+Array.from(boxes).forEach(cur => cur.style.backgroundColor = 'dodgerblue');
+
+// ES5 
+/*
+for (var i = 0; i < boxesArr5.length; i++) {
+    if(boxesArr5[i].className === 'box blue') {
+        continue;
+       //break;
+    }
+    boxesArr5[i].textContent = 'I changed to blue!';
+
+}
+*/
+
+// ES6
+/*
+for (const el of boxesArr6) {
+    if (el.className.includes('blue')) {
+        continue;
+    }
+    el.textContent = 'I changed to blue!';
+}
+*/
+
+
+// ES5 
+
+var ages = [12, 17, 8, 21, 14, 11];
+
+/*
+var full = ages.map(function(el) {
+    return el >= 18;
+});
+console.log(full);
+
+console.log(full.indexOf(true));
+console.log(ages[full.indexOf(true)]);
+*/
 
 
 
+// ES6
 
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(ages.findIndex(el => el >= 18));
+console.log(ages.find(el => el >= 18));
 
 
